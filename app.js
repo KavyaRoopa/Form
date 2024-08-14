164ce887ca4eb744af8af1051204e4b81a52027f
Kavya <kavyathokala392@gmail.com>
2024-08-14 13:03:56 +0530

    files added

---
+++ app.js
@@ -0,0 +1,7 @@
+function fun(){
+var a=document.getElementById("name").value;
+var b=document.getElementById("pwd").value;
+alert(a+" "+b)
+console.log("Hi "+a+" How are you???")
+document.write("hey cutie")
+}
